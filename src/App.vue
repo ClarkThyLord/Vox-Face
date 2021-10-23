babcd567369f7a96d4aa5d10cb79965ecaaf7718
Christian Cuevas <ccpotential@gmail.com>
2021-10-22 21:25:25 -0700

    Clean CSS

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <canvas width="600" height="600" id="jeeFaceFilterCanvas"></canvas>
+  <canvas id="jeeFaceFilterCanvas"></canvas>
 
-  <canvas width="600" height="600" id="threeCanvas"></canvas>
+  <canvas id="threeCanvas"></canvas>
 
-  <canvas width="300" height="300" id="faceCanvas"></canvas>
-  <img src="" alt="FACE" id="faceImage" />
+  <canvas id="faceCanvas"></canvas>
+  <img alt="" id="faceImage" />
 
   <a href="https://github.com/ClarkThyLord/Vox-Face">
-    <img src="../public/vox-face.svg" alt="" class="vf-icon" />
+    <img src="../public/vox-face.svg" alt="Vox-Face" class="vf-icon" />
   </a>
 </template>
 
@@ -130,15 +130,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
 .vf-icon {
   position: absolute;
   bottom: 0px;
@@ -148,66 +139,43 @@ export default {
   z-index: 10000;
 }
 
-a {
-  color: #eee;
-  text-decoration: none;
-}
-a:hover {
-  color: blue;
-}
-body {
-  overflow: auto;
-  overflow-y: auto;
-  background-color: white;
-  background-attachment: fixed;
-  background-position: center;
-  background-size: contain;
-  margin: 0px;
-}
-
 #jeeFaceFilterCanvas {
-  z-index: 10;
-  position: absolute;
-  max-height: 100%;
-  max-width: 100%;
-  left: 50%;
-  top: 50%;
-  width: 100vmin;
-  transform: translate(-50%, -50%) rotateY(180deg);
-}
-
-#threeCanvas {
-  z-index: 11;
-  position: absolute;
-  max-height: 100%;
-  max-width: 100%;
-  left: 50%;
-  top: 50%;
-  width: 100vmin;
-  transform: translate(-50%, -50%) rotateY(180deg);
-}
-
-@media (max-width: 787px) {
-  #jeeFaceFilterCanvas {
-    right: 0px;
-    top: 60px;
-    transform: rotateY(180deg);
-  }
-}
-
-#faceCanvas {
+  z-index: 0;
   position: absolute;
   left: 0px;
   top: 0px;
-  z-index: 100000;
+  width: 100%;
+  height: 100%;
+  transform: rotateY(180deg);
+}
+
+#threeCanvas {
+  z-index: 1;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  transform: rotateY(180deg);
+}
+
+#faceCanvas {
+  z-index: 2;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  /* width: 100%;
+  height: 100%; */
+  transform: rotateY(180deg);
   background: greenyellow;
 }
 
 #faceImage {
+  z-index: 2;
   position: absolute;
   right: 0px;
   top: 0px;
-  z-index: 100000;
+  transform: rotateY(180deg);
   background: greenyellow;
 }
 </style>
