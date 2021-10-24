649eefc69abcd7809d53102e00cbe55d986759cf
Christian Cuevas <ccpotential@gmail.com>
2021-10-23 23:32:49 -0700

    Using Human

--- src/App.vue
+++ src/App.vue
@@ -26,13 +26,47 @@
   </a>
 
   <About ref="about" />
+
+  <main>
+    <video id="cameraFeed"></video>
+    <canvas id="mainCanvas"></canvas>
+  </main>
 </template>
 
 <script>
-import About from "./components/About.vue";
+import About from "/src/components/About.vue";
 
-import * as THREE from "three/build/three.module.js";
-window.THREE = THREE;
+import Human from "@vladmandic/human/dist/human.esm";
+window.human = new Human({
+  modelBasePath: "models/",
+  backend: "webgl",
+  face: {
+    enabled: true,
+    detector: { rotation: true, maxDetected: 1 },
+    mesh: { enabled: true },
+    iris: { enabled: true },
+    description: { enabled: false },
+    emotion: { enabled: false },
+  },
+  hand: { enabled: false },
+  body: { enabled: false },
+  object: { enabled: false },
+  filter: { enabled: false },
+  gesture: { enabled: false },
+});
+
+import {
+  DoubleSide,
+  Mesh,
+  MeshBasicMaterial,
+  OrthographicCamera,
+  Scene,
+  sRGBEncoding,
+  VideoTexture,
+  WebGLRenderer,
+  BufferGeometry,
+  BufferAttribute,
+} from "three";
 
 export default {
   name: "App",
@@ -50,6 +84,164 @@ export default {
       this.showAbout();
       Cookies.set("seenAbout", true, { expires: 7 });
     }
+
+    const wireframe = false;
+
+    window.mainCanvas = document.getElementById("mainCanvas");
+    let width = 0;
+    let height = 0;
+
+    const renderer = new WebGLRenderer({
+      antialias: true,
+      alpha: true,
+      canvas: window.mainCanvas,
+    });
+    renderer.outputEncoding = sRGBEncoding;
+
+    const camera = new OrthographicCamera();
+
+    const materialWireFrame = new MeshBasicMaterial({
+      color: 0xffaaaa,
+      wireframe: true,
+    });
+    const materialFace = new MeshBasicMaterial({
+      color: 0xffffff,
+      side: DoubleSide,
+      map: null,
+    });
+
+    class FaceGeometry extends BufferGeometry {
+      constructor(triangulation) {
+        super();
+        this.positions = new Float32Array(478 * 3);
+        this.uvs = new Float32Array(478 * 2);
+        this.setAttribute("position", new BufferAttribute(this.positions, 3));
+        this.setAttribute("uv", new BufferAttribute(this.uvs, 2));
+        this.setIndex(triangulation);
+      }
+
+      update(face) {
+        let ptr = 0;
+        for (const p of face.mesh) {
+          this.positions[ptr + 0] = -p[0] + width / 2;
+          this.positions[ptr + 1] = height - p[1] - height / 2;
+          this.positions[ptr + 2] = -p[2];
+          ptr += 3;
+        }
+        ptr = 0;
+        for (const p of face.meshRaw) {
+          this.uvs[ptr + 0] = 0 + p[0];
+          this.uvs[ptr + 1] = 1 - p[1];
+          ptr += 2;
+        }
+        materialFace.map.update();
+        this.attributes.position.needsUpdate = true;
+        this.attributes.uv.needsUpdate = true;
+        this.computeVertexNormals();
+      }
+    }
+
+    const scene = new Scene();
+    const faceGeometry = new FaceGeometry(human.faceTriangulation);
+    const mesh = new Mesh(faceGeometry, materialFace);
+    scene.add(mesh);
+
+    function updateSize() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    const isLive = (input) =>
+      input.srcObject &&
+      input.srcObject.getVideoTracks()[0].readyState === "live" &&
+      input.readyState > 2 &&
+      !input.paused;
+
+    async function render(input) {
+      if (isLive(input)) {
+        if (width !== input.videoWidth || height !== input.videoHeight) {
+          width = input.videoWidth;
+          height = input.videoHeight;
+          camera.left = -width / 2;
+          camera.right = width / 2;
+          camera.top = height / 2;
+          camera.bottom = -height / 2;
+          camera.near = -100;
+          camera.far = 100;
+          // camera.zoom = 2;
+          updateSize();
+        }
+
+        const res = await human.detect(input);
+        if (res?.face?.length > 0) {
+          faceGeometry.update(res.face[0]);
+          mesh.material = materialFace;
+          renderer.autoClear = true;
+          renderer.render(scene, camera);
+
+          if (wireframe) {
+            mesh.material = materialWireFrame;
+            renderer.autoClear = false;
+            renderer.render(scene, camera);
+          }
+        }
+      }
+      requestAnimationFrame(() => render(input));
+    }
+
+    async function setupCameraFeed() {
+      if (!navigator.mediaDevices) return null;
+
+      window.cameraFeed = document.getElementById("cameraFeed");
+      window.mainCanvas.addEventListener("click", () => {
+        if (isLive(window.cameraFeed)) window.cameraFeed.pause();
+        else window.cameraFeed.play();
+      });
+      const constraints = {
+        audio: false,
+        video: {
+          facingMode: "user",
+          resizeMode: "crop-and-scale",
+          width: { ideal: 1920 },
+          aspectRatio: 1920 / 1080,
+        },
+      };
+      // if (window.innerWidth > window.innerHeight)
+      //   constraints.video.width = { ideal: window.innerWidth };
+      // else constraints.video.height = { ideal: window.innerHeight };
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      if (stream) window.cameraFeed.srcObject = stream;
+      else return null;
+
+      return new Promise((resolve) => {
+        window.cameraFeed.onloadeddata = async () => {
+          window.cameraFeed.play();
+          resolve(window.cameraFeed);
+        };
+      });
+    }
+
+    (async function main() {
+      window.addEventListener("unhandledrejection", (evt) => {
+        console.error(evt.reason || evt);
+        evt.preventDefault();
+      });
+
+      await human.load();
+      const video = await setupCameraFeed();
+      if (video) {
+        updateSize();
+        window.addEventListener("resize", updateSize);
+
+        const videoTexture = new VideoTexture(video);
+        videoTexture.encoding = sRGBEncoding;
+        materialFace.map = videoTexture;
+
+        render(video);
+      }
+    })();
   },
   methods: {
     showAbout() {
@@ -63,6 +255,14 @@ export default {
 </script>
 
 <style>
+html,
+body,
+#app {
+  widows: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
 .options {
   z-index: 10;
   position: absolute;
@@ -71,11 +271,37 @@ export default {
 }
 
 .vf-icon {
+  z-index: 10000;
   position: absolute;
   bottom: 0px;
   right: 0px;
   width: 64px;
   height: 64px;
-  z-index: 10000;
+}
+
+main {
+  z-index: 0;
+  position: absolute;
+  top: 0;
+  min-height: 100%;
+  min-width: 100%;
+}
+
+#cameraFeed {
+  z-index: 0;
+  left: 50%;
+  position: absolute;
+  min-height: 100%;
+  min-width: 100%;
+  transform: translate(-50%, 0) rotateY(180deg);
+}
+
+#mainCanvas {
+  z-index: 1;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
 }
 </style>
